97c0ae214c5ae60fda312c0be217f7335fb00018
xBioTech <biotech9261@gmail.com>
2023-07-23 16:29:40 +0200

    feat: Add home section

--- src/components/homeSection/home.js
+++ src/components/homeSection/home.js
@@ -1,21 +1,38 @@
+import homeIcon from "../../assets/home-plus.svg";
+import plusIcon from "../../assets/plus-circle.svg";
+
 function createHome() {
   const contentDiv = document.getElementById("content");
   const homeDiv = document.createElement("div");
   homeDiv.classList.add("home");
 
+  const homeHeadlineContainer = document.createElement("div");
+  homeHeadlineContainer.classList.add("headline-container");
+
+  const homeSvg = new Image();
+  homeSvg.src = homeIcon;
+  homeSvg.classList.add("main-home-icon");
+  homeHeadlineContainer.appendChild(homeSvg);
+
   const homeHeadline = document.createElement("h1");
   homeHeadline.classList.add("home-headline");
   homeHeadline.textContent = "Home";
-  homeDiv.appendChild(homeHeadline);
+  homeHeadlineContainer.appendChild(homeHeadline);
+  homeDiv.appendChild(homeHeadlineContainer);
+
+  const homeButtonDiv = document.createElement("div");
+  homeButtonDiv.classList.add("home-btn-div");
 
   const homeParagraph = document.createElement("p");
   homeParagraph.textContent = "Add your default tasks here";
-  homeDiv.appendChild(homeParagraph);
+  homeButtonDiv.appendChild(homeParagraph);
 
-  const homeButton = document.createElement("button");
-  homeButton.classList.add("home-btn");
-  homeButton.textContent = "Add Task";
-  homeDiv.appendChild(homeButton);
+  const plusSvg = new Image();
+  plusSvg.src = plusIcon;
+  plusSvg.classList.add("main-plus-icon");
+  homeButtonDiv.appendChild(plusSvg);
+
+  homeDiv.appendChild(homeButtonDiv);
 
   contentDiv.appendChild(homeDiv);
 }
