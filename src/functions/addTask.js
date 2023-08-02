af42bb85e804887104c21a84fc61910c39a3d3b8
xBioTech <biotech9261@gmail.com>
2023-08-02 16:14:04 +0200

    feat: Add function to select the form fields, extract the values, create a new Task with those values and push the new Object to an array

--- src/functions/addTask.js
+++ src/functions/addTask.js
@@ -8,3 +8,28 @@ class Task {
     this.priority = priority;
   }
 }
+
+function addTaskToTodoListArray(e) {
+  e.preventDefault();
+
+  const formTitleInput = document.getElementById("title");
+  const formDescriptionInput = document.getElementById("description");
+  const formDuedateInput = document.getElementById("duedate");
+  const formPriorityInput = document.getElementById("priority");
+
+  const title = formTitleInput.value;
+  const description = formDescriptionInput.value;
+  const duedate = formDuedateInput.value;
+  const priority = formPriorityInput.value;
+
+  const newTask = new Task(title, description, duedate, priority);
+
+  todoList.push(newTask);
+
+  formTitleInput.value = "";
+  formDescriptionInput.value = "";
+  formDuedateInput.value = "";
+  formPriorityInput.value = "";
+}
+
+function addTaskToUl() {}
