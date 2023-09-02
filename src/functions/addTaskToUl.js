ced52a0254fa87204ea1fa23107b248ebaf1c605
xBioTech <biotech9261@gmail.com>
2023-09-02 18:25:54 +0200

    feat: Add show more and delete task icon

--- src/functions/addTaskToUl.js
+++ src/functions/addTaskToUl.js
@@ -1,26 +1,36 @@
+import viewMoreIcon from "../assets/dots-vertical.svg";
+import deleteIcon from "../assets/minus-circle.svg";
 
-function addTaskToUl(task){
-  
-    const taskUl = document.querySelector(".task-ul");
+function addTaskToUl(task) {
+  const taskUl = document.querySelector(".task-ul");
 
-    const taskLi = document.createElement("li")
-    taskLi.classList.add("task-li")
+  const taskLi = document.createElement("li");
+  taskLi.classList.add("task-li");
 
-    const taskTitleParagrph = document.createElement("p")
-    taskTitleParagrph.textContent = task.title
-    taskLi.appendChild(taskTitleParagrph)
+  const taskTitleParagrph = document.createElement("p");
+  taskTitleParagrph.textContent = task.title;
+  taskLi.appendChild(taskTitleParagrph);
 
-    const taskDueDateParagraph = document.createElement("p")
-    taskDueDateParagraph.textContent = task.dueDate
-    taskLi.appendChild(taskDueDateParagraph)
+  const taskDueDateParagraph = document.createElement("p");
+  taskDueDateParagraph.textContent = task.dueDate;
+  taskLi.appendChild(taskDueDateParagraph);
 
-    const taskButton = document.createElement("button")
-    taskButton.textContent = "X"
-    taskLi.appendChild(taskButton)
+  const iconsDiv = document.createElement("div");
+  iconsDiv.classList.add("icons-div");
 
-    taskUl.appendChild(taskLi)
- 
+  const viewMoreSvg = new Image();
+  viewMoreSvg.src = viewMoreIcon;
+  viewMoreSvg.classList.add("show-more");
+  iconsDiv.appendChild(viewMoreSvg);
+
+  const deleteSvg = new Image();
+  deleteSvg.src = deleteIcon;
+  deleteSvg.classList.add("delete-btn");
+  iconsDiv.appendChild(deleteSvg);
+
+  taskLi.appendChild(iconsDiv);
+
+  taskUl.appendChild(taskLi);
 }
 
-
-export default addTaskToUl
+export default addTaskToUl;
