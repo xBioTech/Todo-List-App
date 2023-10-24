12f9af66f991a6725267761a747e3c7a87c288c1
xbiotech <biotech9261@gmail.com>
2023-10-24 16:46:09 +0200

    feat: Add icons to project list elements in the nav

--- src/functions/addProjectToUl.js
+++ src/functions/addProjectToUl.js
@@ -1,13 +1,34 @@
 import { attachProjectNameEventListeners } from "./eventHandler";
+import wrenchSvg from "../assets/wrench-cog-outline.svg";
+import deleteSvg from "../assets/minus-circle.svg"
 
 function addProjectToUl(project) {
   const projectsUl = document.querySelector(".projects-ul");
 
+  const container = document.createElement("div");
+  container.classList.add("project");
+
   const projectLi = document.createElement("li");
   projectLi.classList.add("custom-project", project.name);
-  projectLi.textContent = project.name;
 
-  projectsUl.appendChild(projectLi);
+  const projectTitle = document.createElement("p");
+  projectTitle.textContent = project.name
+  projectLi.appendChild(projectTitle)
+
+  const wrenchIcon = new Image();
+  wrenchIcon.src = wrenchSvg;
+  projectLi.appendChild(wrenchIcon);
+
+  container.appendChild(projectLi);
+
+  const deleteIcon = new Image();
+  deleteIcon.src = deleteSvg;
+  deleteIcon.classList.add("delete-project-btn");
+
+  container.appendChild(deleteIcon);
+
+  projectsUl.appendChild(container);
+
   attachProjectNameEventListeners();
 }
 
